d7d46a4b9cb4d60db61913de19864b8e58a26cd2
Sreejesh <sreejeshsj01@gmail.com>
2025-07-06 23:04:29 +0530

    completed place order

---
+++ frontend/src/components/CartTotal.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title'
+function CartTotal() {
+    const {currency,delivery_fee,getTotalPrice}=useContext(ShopContext)
+  return (
+    <div className='w-full'>
+      <div className='text-2xl'>
+            <Title text1={'CART'} text2={'TOTAL'}/>
+      </div>
+      <div className='flex flex-col gap-2 mt-2 text-sm'>
+            <div className='flex justify-between'>
+                    <p>Subtotal</p>
+                    <p>{currency} {getTotalPrice()}.00</p>
+            </div>
+            <hr />
+            <div className='flex justify-between'>
+                <p>Shipping Fee</p>
+                <p>{currency} {delivery_fee}.00</p>
+            </div>
+            <hr />
+            <div className='flex justify-between'>
+                <b>Total</b>
+                <b>{currency} {getTotalPrice() === 0 ? 0 : getTotalPrice() + delivery_fee}.00</b>
+            </div>
+      </div>
+    </div>
+  )
+}
+
+export default CartTotal
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import {useNavigate} from 'react-router-dom'
 export const ShopContext=createContext();
 
 const ShopContextProvider= (props)=>{
@@ -9,7 +10,7 @@ const ShopContextProvider= (props)=>{
     const [search,setSearch]=useState('')
     const [showSearch,setShowSearch]=useState(false)
     const [cartItems,setCartItems]=useState({})
-
+    const navigate=useNavigate()
     const addToCart=async (itemId,size)=>{
         if(!size){
             toast.error('Please Select Size')
@@ -53,12 +54,31 @@ const ShopContextProvider= (props)=>{
   }
 
   
+    const getTotalPrice=()=>{
+       let  totalPrice=0
+       for(const items in cartItems){
+        let productInfo=products.find((product)=>product._id===items)
+        for(const item in cartItems[items]){
+            try{
+                if(cartItems[items][item]){
+                    totalPrice=totalPrice+(cartItems[items][item]*productInfo.price)
+                }
+            }catch(err){
+                console.log(err)
+            }
+           
+       }
+    }
+       return totalPrice
+    }
+
     
     const value = {
         products,currency,delivery_fee,
         search,setSearch,showSearch,setShowSearch,
         cartItems,addToCart,
-        getCartCount,updateQuantity
+        getCartCount,updateQuantity,
+        getTotalPrice,navigate
 
     }
     return (
