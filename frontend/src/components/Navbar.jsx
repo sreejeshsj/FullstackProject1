55b92a0ff5a8b13d5676d4f91459fe2c381b9dc3
Sreejesh <sreejeshsj01@gmail.com>
2025-07-07 14:43:49 +0530

    Login and Order completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { assets } from "../assets/assets";
 import { ShopContext } from "../context/ShopContext";
 function Navbar() {
     const [visible,setVisible]=useState(false)
+    
     const {setShowSearch,getCartCount}=useContext(ShopContext)
   return (
     <div className="flex items-center justify-between py-5 font-medium">
@@ -32,14 +33,16 @@ function Navbar() {
       <div className="flex items-center gap-6">
         <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
         <div className="group relative">
+          <Link to='/login'>
           <img
             src={assets.profile_icon}
             className="w-5 cursor-pointer"
             alt=""
           />
+          </Link>
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
-              <p className="cursor-pointer hover:text-black">My Profile</p>
+              <p  className="cursor-pointer hover:text-black">My Profile</p>
 
               <p className="cursor-pointer hover:text-black">Orders</p>
               <p className="cursor-pointer hover:text-black">Logout</p>
