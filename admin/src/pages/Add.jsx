7262df359e6c66b210a1f3be39e7f62ddd8e4c82
Sreejesh <sreejeshsj01@gmail.com>
2025-07-09 14:42:03 +0530

    Product adding functionality completed

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,22 +1,50 @@
 import React, { useState } from 'react'
 import { assets } from '../assets/assets'
-
-function Add() {
+import axios from 'axios'
+import { backendUrl } from '../App'
+function Add({token}) {
   const [image1,setImage1]=useState(false)
   const [image2,setImage2]=useState(false)
   const [image3,setImage3]=useState(false)
   const [image4,setImage4]=useState(false)
 
   const [name,setName]=useState("")
-  const [descriptions,setDescription]=useState("")
+  const [description,setDescription]=useState("")
   const [price,setPrice]=useState("")
   const [category,setCategory]=useState('Men')
-  const [subcategory,setSubCategory]=useState('Topwear')
-  const [bestseller,setBestseller]=useState(false)
+  const [subCategory,setSubCategory]=useState('Topwear')
+  const [bestSeller,setBestseller]=useState(false)
   const [sizes,setSizes]=useState([])
+  
+  const handleSubmit=async(e)=>{
+        e.preventDefault()
+        try{
+          const formData= new FormData()
+
+          formData.append("name",name)
+          formData.append("description",description)
+          formData.append("price",price)
+          formData.append("category",category)
+          formData.append("subCategory",subCategory)
+          formData.append("bestSeller",bestSeller)
+          formData.append("sizes",JSON.stringify(sizes))
+          image1 && formData.append("image1",image1)
+          image2 && formData.append("image2",image2)
+          image3 && formData.append("image3",image3)
+          image4 && formData.append("image4",image4)
+          const response=await axios.post(`${backendUrl}/api/product/add`,formData,{
+            headers:{
+              token
+            }
+          })
+          console.log(response)
+        }catch(err){
+          console.log(err)
+        }
+  }
 
   return (
-    <form className='flex flex-col w-full items-start gap-3'>
+    <form onSubmit={handleSubmit} className='flex flex-col w-full items-start gap-3'>
       <div>
         <p className='mb-2'>Upload Image</p>
         <div className='flex gap-2'>
@@ -44,7 +72,7 @@ function Add() {
       </div>
       <div className='w-full'>
         <p className='mb-2'>Product description</p>
-        <textarea onChange={(e)=>setDescription(e.target.value)} value={descriptions} className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Write content here' required/>
+        <textarea onChange={(e)=>setDescription(e.target.value)} value={description} className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Write content here' required/>
       </div>
       <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
         <div>
@@ -57,7 +85,7 @@ function Add() {
         </div>
         <div>
           <p className='mb-2'>Sub category</p>
-          <select onChange={(e)=>setCategory(e.target.value)} className='w-full px-3 py-2'>
+          <select onChange={(e)=>setSubCategory(e.target.value)} className='w-full px-3 py-2'>
             <option value="Topwear">Topwear</option>
             <option value="Bottomwear">Bottomwear</option>
             <option value="Winterwear">Winterwear</option>
@@ -89,7 +117,7 @@ function Add() {
         </div>
       </div>
       <div className='flex gap-2 mt-2'>
-        <input onChange={()=>setBestseller(prev=>!prev)} checked={bestseller} type="checkbox"  id="bestseller" />
+        <input onChange={()=>setBestseller(prev=>!prev)} checked={bestSeller} type="checkbox"  id="bestseller" />
         <label className='cursor-pointer' htmlFor="bestseller">Add to bestseller</label>
       </div>
       <button type='submit' className='w-28 py-3 mt-4 bg-black text-white'>Add</button>
